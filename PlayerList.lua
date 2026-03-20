3a890a250d21472d806a3ddf9e5153f72111450e
emirontop3 <ozl.kpz@gmail.com>
2026-03-20 23:09:13 +0300

    GUI Upload: PlayerList

---
+++ PlayerList.lua
@@ -0,0 +1,25 @@
+-- Exported GUI: PlayerList
+-- Date: Fri Mar 20 23:09:13 2026
+
+local TweenService = game:GetService('TweenService')
+local parent = game:GetService('Players').LocalPlayer:WaitForChild('PlayerGui')
+
+local v_PlayerList_510 = Instance.new('ScreenGui')
+v_PlayerList_510.Name = [[PlayerList]]
+v_PlayerList_510.Parent = parent
+do
+    local parent = v_PlayerList_510
+local v_FoundationCursorContainer_704 = Instance.new('Frame')
+v_FoundationCursorContainer_704.Name = [[FoundationCursorContainer]]
+v_FoundationCursorContainer_704.Size = UDim2.new(0.000000, 0, 0.000000, 0)
+v_FoundationCursorContainer_704.Position = UDim2.new(0.000000, 0, 0.000000, 0)
+v_FoundationCursorContainer_704.BackgroundColor3 = Color3.new(0.639216, 0.635294, 0.647059)
+v_FoundationCursorContainer_704.BackgroundTransparency = 0
+v_FoundationCursorContainer_704.BorderSizePixel = 1
+v_FoundationCursorContainer_704.Visible = false
+v_FoundationCursorContainer_704.ZIndex = 1
+v_FoundationCursorContainer_704.Parent = parent
+do
+    local parent = v_FoundationCursorContainer_704
+end
+end
